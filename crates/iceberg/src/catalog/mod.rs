b11e58ab1154d0786fe9ba03eacf228fb71145ff
dentiny <dentinyhao@gmail.com>
2025-05-14 10:11:06 +0000

    allow mock catalog

--- crates/iceberg/src/catalog/mod.rs
+++ crates/iceberg/src/catalog/mod.rs
@@ -24,6 +24,8 @@ use std::ops::Deref;
 
 use _serde::deserialize_snapshot;
 use async_trait::async_trait;
+#[cfg(test)]
+use mockall::{automock, predicate::*};
 use serde_derive::{Deserialize, Serialize};
 use typed_builder::TypedBuilder;
 use uuid::Uuid;
@@ -38,6 +40,7 @@ use crate::{Error, ErrorKind, Result};
 
 /// The catalog API for Iceberg Rust.
 #[async_trait]
+#[cfg_attr(test, automock)]
 pub trait Catalog: Debug + Sync + Send {
     /// List namespaces inside the catalog.
     async fn list_namespaces(&self, parent: Option<&NamespaceIdent>)
